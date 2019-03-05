2fc3feb2ce645f37ac9d9121079e2935152f9b34
SAMAEL NEWGATE <SAMNEWGA@uat.edu>
2019-03-04 23:11:28 -0700

    final version

--- 08ml.py
+++ 08ml.py
@@ -18,9 +18,24 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.5)
 my_decision_tree_classifier = tree.DecisionTreeClassifier()
 
 print("Welcome to the Iris Data Set Classifying Program.")
-print("\nHow it works:\nIt imports the iris data set from sklearn.")
-print("\nData set explanation:\nThe data set is broken down into rows with 4 different values that apply to one specific flower.")
-print("\nData set legend: Row[1st value = sepal lenght(cm), 2nd value = sepal width(cm), 3nd value = petal lenght(cm), 4th value = petal width(cm)")
+print("\nHow it works:\nIt imports the iris data set from sklearn that has specific values that the classifiers use to learn how to distiguish the flower species.")
+print("\nData set explanation:\nThe data set is broken down into two parts. "
+      "\nPart one contain 4 values that belong to a specific flower"
+      "\nPart two contains the flower label for those part one values.")
+
+
+# Asking user for input to display part 21 of the two part training dataset
+wait = input("\nPress enter to view first part of the data set used for training.")
+print("="*80)
+print("\nData set legend: \nRow [1st value = sepal lenght(cm), 2nd value = sepal width(cm), 3nd value = petal lenght(cm), 4th value = petal width(cm)\n")
+print(x_train)
+print("="*80)
+# Asking user for input to display part 2 of the two part training dataset
+wait = input("\nPress enter to view second part of the data set used for training.")
+print("="*80)
+print("\nData set legend:\n0 = Setosa, 1 = Versicolor, 2 = Virginica")
+print(y_train)
+print("="*80)
 
 # Using x_train and y_train to train the decision tree classifier
 my_decision_tree_classifier.fit(x_train, y_train)
@@ -28,32 +43,41 @@ my_decision_tree_classifier.fit(x_train, y_train)
 # Using .predict to predict our input of x_test
 predictions_from_decision_tree_classifier = my_decision_tree_classifier.predict(x_test)
 
-# Let's print out the predictions
-# These correspond to the type of iris predicted
-# for each row of our testing data
+# Asking user for input to display the dataset used for testing
+print("\n\nNow we input a data set for our two classifiers to test their predictions.")
+wait = input("\nPress enter to display the data set used for testing and input the data set")
+print("="*80)
+print("\nData set legend: \nRow [1st value = sepal lenght(cm), 2nd value = sepal width(cm), 3nd value = petal lenght(cm), 4th value = petal width(cm)\n")
+print(x_test)
+print("="*80)
 
-print("Here we print out our prediction.")
-print("These correspond to the type of iris predicted.")
-print("For each row of our testing data.")
-print("")
-
-# Now lets see how accurate our classifier was on the testing data
-# To calculate our accuracy, we can compare our predicted labels with the true labels
-# from sklearn.metrics import accuracy_score
-print("The predictions from the decision tree classifier")
+# Asking user for input to print out the predictions and accuracy of our first classifier, the decision tree classifier from sklearn
+wait = input("Press enter to view results of first classifier")
+print("="*80)
+print("Classifier 1 results:")
 print(predictions_from_decision_tree_classifier)
+print("\nClassifier 1 accuracy:")
 print(accuracy_score(y_test, predictions_from_decision_tree_classifier))
+print("="*80)
 
-############# implement the KNN ##################
 
+# Importing our second classifier, KNeighborsClassifier
 from sklearn.neighbors import KNeighborsClassifier
 my_k_nearest_neighbors_classifier = KNeighborsClassifier()
 
+# Using x_train and y_train again to train our second classifier
 my_k_nearest_neighbors_classifier.fit(x_train, y_train)
 
+# Using the x_test input to do our predictions
 predictions_from_KNeighborsClassifier = my_k_nearest_neighbors_classifier.predict(x_test)
-print("The predictions from KNN classifier list")
 
+# Asking user for input to pint out the predictions and accuracy of our second classifier, the k neighbors classifier from sklearn
+wait = input("Press enter to view results of first classifier")
+print("="*80)
+print("Classifier 2 results:")
+print(predictions_from_KNeighborsClassifier)
+print("\nClassifier 2 accuracy:")
 print(accuracy_score(y_test, predictions_from_KNeighborsClassifier))
+print("="*80)
 
 
